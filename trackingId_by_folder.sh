046415ed7ca255661a2800ec0729d9df412e0fff
Geoff Golder <geoff@ixisdigital.com>
2016-06-15 15:40:19 -0400

    adding ability to list folders and tracking ids

---
+++ trackingId_by_folder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage(){
+  echo "Prints all tag names for export from GTM"
+  echo "$0 GTM-XXXX.json folder_id";
+  echo "Note, you can get the folder ID by using ./list_folders.sh"
+  exit 1
+}
+
+require_jq(){
+  echo "$0 Requires the json parsing utility jq"
+  echo "See https://stedolan.github.io/jq/ or install with your favorite package manager"
+  exit 1
+}
+
+[ $# -lt 2 ] && {
+  usage
+}
+
+if ! [ -x "$(command -v jq)" ]; then
+  require_jq
+fi
+  
+cat $1 | jq '.containerVersion.tag | .[] | select(.parentFolderId == "308") | .parameter | .[] | select(.key=="trackingId")'
